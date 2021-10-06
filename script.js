41249a2511f58f028387ffd82423d9b241e1245e
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-06 15:33:04 -0700

    created the html canvas element and added it to the DOM

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ const downloadBtn = document.getElementById('download');
 const { body } = document;
 
 // Global Variables
-
-let currentSize = 10;
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+const context = canvas.getContext('2d');
+let currentSize = 10; //Brush size or Eraser size
 let bucketColor = '#FFFFFF';
 let currentColor = '#A51DAB';
 // let isEraser = false;
@@ -124,11 +126,11 @@ canvas.addEventListener('mousedown', (event) => {
   isMouseDown = true;
   const currentPosition = getMousePosition(event);
   console.log('mouse is clicked', currentPosition);
-//   context.moveTo(currentPosition.x, currentPosition.y);
-//   context.beginPath();
-//   context.lineWidth = currentSize;
-//   context.lineCap = 'round';
-//   context.strokeStyle = currentColor;
+  //   context.moveTo(currentPosition.x, currentPosition.y);
+  //   context.beginPath();
+  //   context.lineWidth = currentSize;
+  //   context.lineCap = 'round';
+  //   context.strokeStyle = currentColor;
 });
 
 // Mouse Move
@@ -136,17 +138,17 @@ canvas.addEventListener('mousemove', (event) => {
   if (isMouseDown) {
     const currentPosition = getMousePosition(event);
     console.log('mouse is moving', currentPosition);
-  //   context.lineTo(currentPosition.x, currentPosition.y);
-  //   context.stroke();
-  //   storeDrawn(
-  //     currentPosition.x,
-  //     currentPosition.y,
-  //     currentSize,
-  //     currentColor,
-  //     isEraser,
-  //   );
-  // } else {
-  //   storeDrawn(undefined);
+    //   context.lineTo(currentPosition.x, currentPosition.y);
+    //   context.stroke();
+    //   storeDrawn(
+    //     currentPosition.x,
+    //     currentPosition.y,
+    //     currentSize,
+    //     currentColor,
+    //     isEraser,
+    //   );
+    // } else {
+    //   storeDrawn(undefined);
   }
 });
 
